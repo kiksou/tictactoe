88bbfcfd00c3086ae511a3ec7c1ea92dd8675c6c
Avi-Mkikers <uzan99@hotmail.fr>
2022-01-05 13:25:54 +0100

    correction css + game.js

--- tictactoe/game.js
+++ tictactoe/game.js
@@ -1,21 +1,16 @@
 const board_el= document.querySelector('#board');
 
-const cell_els = document.querySelectorAll('#board .cell'); const combinations = [
+const cell_els = document.querySelectorAll('#board .cell'); 
 
-[0, 1, 2], I
+const combinations = [
 
+[0, 1, 2], 
 [3, 4, 5],
-
 [6, 7, 8],
-
 [0, 3, 6],
-
 [1, 4, 7],
-
 [2, 5, 8],
-
 [0, 4, 8],
-
 [2, 4, 6]
 
 ];
